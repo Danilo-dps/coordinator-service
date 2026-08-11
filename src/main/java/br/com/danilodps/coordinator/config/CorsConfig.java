a50fd17c33166b05df2f274bbfb8de07b9e2244a
Danilo Pereira da Silva <danilodpsnilo@outlook.com>
2026-08-10 21:45:29 -0300

    feat: adição do cors config ao yaml

--- src/main/java/br/com/danilodps/coordinator/config/CorsConfig.java
+++ src/main/java/br/com/danilodps/coordinator/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package br.com.danilodps.coordinator.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,11 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${web.cors.allowed-origins}")
+    private String[] allowedOrigins;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
+
 }
